900b91cbc3c826b4656b9d771505ab64c4a2a039
VirtuousCrane <ray.daroonpat.2020@gmail.com>
2023-10-02 17:49:15 +0700

    Modified: bluetooth detection to use ChangeNotifier Instead

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:ble_test/bluetooth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,64 +8,28 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a blue toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: MyHomePage(),
-      ),
+    return const MaterialApp(
+      title: "BLE Test",
+      home: MyHomePage(),
     );
   }
-}
 
-class BLEDevice {
-  String id;
-  int rssi;
-  
-  BLEDevice(this.id, this.rssi);
-}
-
-class MyAppState extends ChangeNotifier {
-  // To Implement
-  final flutterReactiveBle = FlutterReactiveBle();
-  var devices = <BLEDevice>[];
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final bluetoothNotifier = BluetoothNotifier();
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var flutterReactiveBle = appState.flutterReactiveBle;
-    var devices = appState.devices;
-    var bleStream = flutterReactiveBle.scanForDevices(withServices: [], scanMode: ScanMode.lowLatency);
-    
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -75,28 +38,22 @@ class _MyHomePageState extends State<MyHomePage> {
             ElevatedButton(
               onPressed: () {
                 print("Scanning...");
-
-                var subscription = bleStream.listen((data) {
-                  setState(() {
-                    var idList = devices.map((e) => e.id).toList();
-                    var idIdx = idList.indexOf(data.id);
-                    if (idIdx != -1) {
-                      devices[idIdx].rssi = data.rssi;
-                    } else {
-                      devices.add(BLEDevice(data.id, data.rssi));
-                    }
-                  });
-                }, onError: (error) {
-                  print("Scan Failed: ${error.toString()}");
-                }, cancelOnError: true);
+                bluetoothNotifier.scan();
               },
               child: Text('Scan'),
             ),
             Expanded(
-              child: ListView(children: [
-                for (final device in devices)
-                  ListTile(title: Text(device.id), subtitle: Text(device.rssi.toString()),)
-              ],)
+              child: ListenableBuilder(
+                listenable: bluetoothNotifier,
+                builder: (context, child) {
+                  return ListView(
+                    children: [
+                      for (final device in bluetoothNotifier.devices)
+                        ListTile(title: Text(device.id), subtitle: Text(device.rssi.toString()))
+                    ],
+                  );
+                },
+              ),
             ),
           ],
         )
